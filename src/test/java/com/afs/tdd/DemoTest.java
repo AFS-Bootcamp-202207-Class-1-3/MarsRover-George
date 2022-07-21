723f03fe132560e1b32a8f90cde93d8e41cffd45
chan <1911270452@qq.com>
2022-07-21 21:04:34 +0800

    test:implement should_return_1_0_E_when_execute_command_given_0_0_E_and_M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -96,6 +96,19 @@ class DemoTest {
         assertEquals(expectedLocation, marsRover.getLocation());
     }
 
+    @Test
+    void should_return_1_0_E_when_execute_command_given_0_0_E_and_M() {
+        //given
+        Location location = new Location(0,0,"E");
+        String command = "M";
+        Location expectedLocation = new Location(1,0,"E");
 
+        //when
+        MarsRover marsRover = new MarsRover(location);
+        marsRover.executeCommand(command);
+
+        //then
+        assertEquals(expectedLocation, marsRover.getLocation());
+    }
 
 }
