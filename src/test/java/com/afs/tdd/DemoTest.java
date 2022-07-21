085a93b26f04eae27ab1c7c6b011be8b950da186
CHENGE8 <1911270452@qq.com>
2022-07-21 16:27:21 +0800

    test:implement should_return_0_1_N_when_execute_command_given_0_0_N_and_M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,28 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_return_0_1_N_when_execute_command_given_0_0_N_and_M() {
+        //given
+        int locationX = 0;
+        int locationY = 0;
+        String direction = "N";
+        String command = "M";
 
+        int expectedLocationX = 0;
+        int expectedLocationY = 1;
+        String expectedDirection = "N";
+
+        //when
+        MarsRover marsRover = new MarsRover(locationX, locationY, direction);
+        marsRover.executeCommand(command);
+
+        //then
+        assertEquals(expectedLocationX, marsRover.getLocationX());
+        assertEquals(expectedLocationY, marsRover.getLocationY());
+        assertEquals(expectedDirection, marsRover.getDirection());
     }
 }
